f6be1e96ae1d53badb292354f4df6b7069bfbfba
Pedro Suchite <pedsuch@gmail.com>
2019-02-15 22:17:57 -0800

    Initial Commit

---
+++ Calculator App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator App
+//
+//  Created by Pedro Suchite on 2/15/19.
+//  Copyright © 2019 Pedro Suchite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
